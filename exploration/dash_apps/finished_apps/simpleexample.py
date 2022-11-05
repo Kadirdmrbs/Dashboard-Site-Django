d1e2fce5edf4dfe7845736992577088c1b22c7d8
Kadirdmrbs <kadird39@gmail.com>
2022-11-05 15:39:35 +0300

    path2

--- exploration/dash_apps/finished_apps/simpleexample.py
+++ exploration/dash_apps/finished_apps/simpleexample.py
@@ -3,6 +3,8 @@ import pandas as pd
 import plotly.express as px
 from django_plotly_dash import DjangoDash
 import os
+from ...functions.featen import fe_data
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -10,11 +12,13 @@ app = DjangoDash('SimpleExample', external_stylesheets=external_stylesheets)
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-df = pd.read_csv('video_data_04-11-22.csv')
+df = pd.read_csv('../../functions/video_data_04-11-22.csv')
 
+new_df = fe_data(df)
+table = new_df[['Id','Title','Date','Comments','Likes','Views','DurationinSeconds','Categories','SubCategory']]
 
-app.layout = dash_table.DataTable(df.to_dict('records'), 
-[{"name": i, "id": i} for i in df.columns],
+app.layout = dash_table.DataTable(new_df.to_dict('records'), 
+[{"name": i, "id": i} for i in table.columns],
 page_size=10)
 
 
